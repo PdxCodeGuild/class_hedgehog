4d78dc332bf7081ae76d52b958d219790f76ff62
Zane Ashby <zanecashby@gmail.com>
2022-05-25 23:46:17 -0400

    Finished lab_02b

--- code/ashby/python/Lab_02b.1.py
+++ code/ashby/python/Lab_02b.1.py
@@ -20,17 +20,21 @@ value = float(input("Hello! please enter a dollar amount in the format of '0.00'
 while value < 0.01 :
     value = float(input("Please choose a positive value, or a value greater than 0. Also, no fractions of a penny please: "))
 value = value*100
-print(value)
-#various coin value calculations. Omitted anything higher than half-follar.
-coin_quantity['half-dollar'] = int(value //coin_list[1][1])
-value %= coin_list[1][1]  
-coin_quantity['quarter'] = int(value //coin_list[2][1])
-value %= coin_list[2][1]            
-coin_quantity['dime'] = int(value //coin_list[3][1])
-value %= coin_list[3][1]   
-coin_quantity['nickel'] = int(value //coin_list[4][1])
-value %= coin_list[4][1]  
-coin_quantity['penny'] = int(value //coin_list[5][1])
-value %= coin_list[5][1]
+#various coin value calculations. 
 
-print(f"{coin_quantity['half-dollar']} half-dollar(s), {coin_quantity['quarter']} quarter(s), {coin_quantity['dime']} dime(s), {coin_quantity['nickel']} nickel(s), {coin_quantity['penny']} {'penny' if coin_quantity['penny']==1 else 'pennies'}.")
+for x in coin_list:
+    coin_quantity[x[0]] = int(value // x[1])
+    value %= x[1]
+
+#Old code for reference
+
+# coin_quantity['quarter'] = int(value //coin_list[2][1])
+# value %= coin_list[2][1]            
+# coin_quantity['dime'] = int(value //coin_list[3][1])
+# value %= coin_list[3][1]   
+# coin_quantity['nickel'] = int(value //coin_list[4][1])
+# value %= coin_list[4][1]  
+# coin_quantity['penny'] = int(value //coin_list[5][1])
+# value %= coin_list[5][1]
+
+print(f"{coin_quantity['golden dollar coin']} golden dollar(s), {coin_quantity['half-dollar']} half-dollar(s), {coin_quantity['quarter']} quarter(s), {coin_quantity['dime']} dime(s), {coin_quantity['nickel']} nickel(s), {coin_quantity['penny']} {'penny' if coin_quantity['penny']==1 else 'pennies'}.")
