7a0e2265bb0ad99c9d77f3ce2aba1a07340dd10e
keenen <newuser@Keenens-MBP.home>
2022-05-25 23:01:07 -0400

    Finished lab 02b

---
+++ code/keenen/python/lab02b.py
@@ -0,0 +1,34 @@
+
+dollar_amount = float(input('Enter a dollar amount: '))
+
+pennies = dollar_amount * 100
+
+# print(pennies)
+
+# how many times does .25 (quarter) go into variable pennies
+
+quarters = pennies // 25
+
+# print(quarters)
+
+after_quarters = pennies - (quarters * 25)
+
+# print(after_quarters)
+
+# how many times does .10 (dime) go into remainder after variable quarters
+
+dimes = after_quarters // 10
+
+# print(dimes)
+
+after_dimes = after_quarters - (dimes * 10)
+
+# print(after_dimes)
+
+pennies_remaining = after_dimes // 1
+
+# print(pennies_remaining)
+
+output = f'{int(quarters)} quarter(s), {int(dimes)} dime(s), and {int(pennies_remaining)} penny(ies)'
+
+print(output)
