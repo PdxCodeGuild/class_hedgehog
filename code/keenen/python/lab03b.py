57178de6af5cf159fed0bb71a43bfead04de154d
keenen <newuser@Keenens-MBP.home>
2022-05-27 00:31:17 -0400

    finished lab03b version 1

---
+++ code/keenen/python/lab03b.py
@@ -0,0 +1,31 @@
+nums = [5, 0, 8, 3, 4, 1, 6]
+
+# loop over the elements
+total = 0
+for num in nums:
+    total = total + num
+    # print(total)
+
+avg = total / len(nums)
+print(f'The average of the numbers list is: {avg}')
+    
+
+
+
+
+
+
+
+
+
+
+
+# loop over the indices
+# for i in range(len(nums)):
+#     print(nums[i])
+
+# def add(nums):
+#     total = 0
+#     for num in nums:
+#     # return total
+# add()
