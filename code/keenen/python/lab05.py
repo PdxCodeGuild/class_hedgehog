7c37c3c457089dcfd12dac9377966e2216661249
keenen <newuser@Keenens-MBP.home>
2022-05-28 16:49:11 -0400

    updated lab 05 w/ defined functions

--- code/keenen/python/lab05.py
+++ code/keenen/python/lab05.py
@@ -2,7 +2,7 @@
 # lab05 - Palindrome and Anagram checker
 # May 27 2022
 
-""" Palindrome Checker """
+""" Palindrome Checker w/o defining function """
 
 # word_1 = "racecar"
 
@@ -14,7 +14,7 @@ else:
     print('False')
 
 
-""" Anagram Checker """
+""" Anagram Checker w/o defining function """
 
 user_1 = input('Enter the first word that you want to check as an anagram: ').lower()
 
@@ -29,3 +29,30 @@ if sorted(user_1_replace) == sorted(user_2_replace):
 
 else:
     print(f"\n'{user_1}' and '{user_2}' are NOT anagrams.\n")
+
+""" Anagram Checker w/ defining function """
+
+def check_anagram(word_1, word_2):
+    word_1 = sorted(word_1)
+    word_2 = sorted(word_2)
+    if word_1 == word_2:
+        return True
+    else:
+        return False
+
+word_1 = input('Enter your first word: ').replace(" ", "").lower()
+word_2 = input('Enter your second word: ').replace(" ", "").lower()
+print(check_anagram(word_1, word_2))
+
+""" Palindrome Checker w/ defining function """
+
+def check_palindrome(word):
+    word = ""
+
+    if word == word [::-1]:
+        return True
+    else:
+        return False
+
+word = input('Enter a word to check if it is a palindrome: ')
+print(check_palindrome(word))
