434ef8c6442085829be4e39edd7fe20cf812bdba
Stacy Green <stacyleegreen@gmail.com>
2022-06-07 11:45:54 +0700

    Added practice05_lists.py, finished

--- code/stacy/python/practice05_lists.py
+++ code/stacy/python/practice05_lists.py
@@ -17,7 +17,6 @@ def test_even_even():
     assert even_even([5, 6, 2]) == True
     assert even_even([5, 5, 2]) == False
 
-
 ## Reverse
 # Write a function that takes a list and returns a new list with the elements in reverse order
 
@@ -71,51 +70,56 @@ def find_pair(nums, target):
 def test_find_pair():
     assert find_pair([5, 6, 2, 3], 7) == [5, 2]
 
-
-
-
 # Average
 # Write a function to find the average of a list of numbers
 
-
 def average(nums):
-    ...
+    sum = 0
+    for num in nums:
+        sum += num
+    average = sum // len(nums)
+    return average
 
 def test_average():
     assert average([1, 2, 3, 4, 5]) == 3
 
-
 # Remove Empty
 # Write a function to remove all empty strings from a list.
 
-
 def remove_empty(mylist):
-    ...
+    new_list = []
+    for item in mylist:
+        if item != '':
+            new_list.append(item)
+    return new_list
+
 def test_remove_empty():
     assert remove_empty(['a', 'b', '', 'c', '', 'd']) == ['a', 'b', 'c', 'd']
-
-
 
 # Merge
 # Write a function that merges two lists into a single list, where each element of the output list is a list containing two elements, one from each of the input lists.
 
 def merge(nums1, nums2):
-    ...
-    
+    merged_list = []
+    for x in range(len(nums1)):
+        pair = []
+        pair.append(nums1[x])
+        pair.append(nums2[x])
+        merged_list.append(pair)
+    return merged_list        
+
 def test_merge():
     assert merge([5,2,1], [6,8,2]) == [[5,6],[2,8],[1,2]]
-
 
 # Combine All
 # Write a function `combine_all` that takes a list of lists, and returns a list containing each element from each of the lists.
 
-
 def combine_all(nums):
-    ...
+    combined_list = []
+    for i in nums:
+        for j in i:
+            combined_list.append(j)
+    return combined_list
     
 def test_combine_all():
     assert combine_all([[5,2,3],[4,5,1],[7,6,3]]) == [5,2,3,4,5,1,7,6,3]
-
-
-
-
