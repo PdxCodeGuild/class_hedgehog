4502a6827e7a40cd3e37c3573b4b80d364a2f942
TheAncientBooer <randall.mayton@gmail.com>
2022-06-07 14:39:03 -0700

    Quick Save  🗡️

--- code/randall/python/test.py
+++ code/randall/python/test.py
@@ -1,1 +1,6 @@
+def latest_letter(word): #passes word in
+    highest_letter = sorted(word)[-1] #creates sorted list in alphabetic order and selects last item in string
+    print(word)
+    return highest_letter #returns the lasr letter in the string
 
+print(latest_letter("poqzmry"))
