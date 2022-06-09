d68ca66087dff4d8ba2a44dc05a9ae9433c46f37
Leigh <LeighUnitedEarth@gmail.com>
2022-06-08 19:37:13 -0700

    finished lab 11

---
+++ code/leigh/python/lab11.py
@@ -0,0 +1,78 @@
+# lab 11
+# Rot Cipher
+# Leigh Fair-Smiley
+# 6/8/2022
+
+def main():
+    english = {
+        0: "a",
+        1: "b",
+        2: "c",
+        3: "d",
+        4: "e",
+        5: "f",
+        6: "g",
+        7: "h",
+        8: "i",
+        9: "j",
+        10: "k",
+        11: "l",
+        12: "m",
+        13: "n",
+        14: "o",
+        15: "p",
+        16: "q",
+        17: "r",
+        18: "s",
+        19: "t",
+        20: "u",
+        21: "v",
+        22: "w",
+        23: "x",
+        24: "y",
+        25: "z"
+    }
+    english_reverse = {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+        "f": 5,
+        "g": 6,
+        "h": 7,
+        "i": 8,
+        "j": 9,
+        "k": 10,
+        "l": 11,
+        "m": 12,
+        "n": 13,
+        "o": 14,
+        "p": 15,
+        "q": 16,
+        "r": 17,
+        "s": 18,
+        "t": 19,
+        "u": 20,
+        "v": 21,
+        "w": 22,
+        "x": 23,
+        "y": 24,
+        "z": 25
+    }
+    input_string = input("Enter a string: ")
+    rot = int(input("Input the amount of ROT: "))
+
+    rot_string = ""
+    for letter in input_string:
+        if letter.isalpha():
+            loc = english_reverse[letter.lower()]
+            rot_string += english[(loc + rot) % 26]
+        else:
+            rot_string += letter
+    
+    print(f"Rot {rot} string output: {rot_string}")
+           
+    ...
+
+main()
