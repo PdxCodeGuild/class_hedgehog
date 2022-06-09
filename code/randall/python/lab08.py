a85aeaa776ced751c360bad4cc50777b59bcbb19
randall <randall.mayton@gmail.com>
2022-06-08 19:55:45 -0700

    Add lab11 🔑

--- code/randall/python/lab08.py
+++ code/randall/python/lab08.py
@@ -22,13 +22,13 @@ def the_picks(the_picks):
     the_picks = [random.randint(1, 99) for x in range(0, 6)]
     balance = 0
     winnings = 0
-    for i in range(100000):
+    for _ in range(100000):
         balance -= 2
         next6 = [random.randint(1, 99) for x in range(0, 6)]
         six = [x for x in range(0,6)]
         chk_res = dict(zip(six, the_picks))
         chk_comp = dict(zip(six, next6))
-        same_num = 0   
+        same_num = 0
         if chk_res[0] == chk_comp[0]:
             same_num += 1
         elif chk_res[1] == chk_comp[1]:
