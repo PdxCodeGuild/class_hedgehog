3707bb6577f2adf1aede56b4a34cef93582200c6
Stacy Green <stacyleegreen@gmail.com>
2022-06-14 11:30:33 +0700

    Added functions for caluclating standard deviation

--- code/stacy/python/minicap_statistical_analysis.py
+++ code/stacy/python/minicap_statistical_analysis.py
@@ -1,5 +1,5 @@
 """
-Let's write a program that calculates and displays 
+Let's write a program that calculates and displays statistical data
 Goals:
     Calculate the distribution of a dataset
     Display that data
@@ -16,52 +16,39 @@ Stretch Goals:
     MANOVA (Multivariant ANOVA)
 """
 
-
-"""
-dataset = [
-    [
-        {
-
-        }
-
-    ], 
-    [
-        {
-
-        }
-
-    ]
-]
-
-"""
-
 dataset = [[], [], []] # List of subject IDs, list of dependent values, list of independent values
 
-def mean(dataset: list):
+def mean(dataset):
+    n = len(dataset)
+    sum = 0
+    for num in dataset:
+        sum += num
+    mean = sum / n
+    return mean
 
-    pass
+def sum_of_squares(mean: float, dataset: list): # mean and list of values
+    sum = 0 
+    for num in dataset:
+        sum += ((num - mean) ** 2)
+    return sum
 
-def sum_of_squares(dataset: list):
+def variance(sum_of_squares: float, dataset: list): # sum of squares and list of values
+    n = len(dataset)
+    variance = sum_of_squares/(n-1)
+    return variance
 
-    pass
-
-def variance(dataset: list):
-
-    pass
-
-def standard_deviation(dataset: list):
-
-    pass
-
+def standard_deviation(variance: float): # variance and list of values
+    standard_deviation = variance ** 1/2
+    return standard_deviation
 
 ######## still not sure how classes work, but I'll just write a skeleton and fix it later
 class Subject:
     # Each subject will have an identifier and values for variables. We'll start with 1 dependent variable
-    def __init__(self, subject: dict, dependent: dict, independent: dict):
-        self.subject_name = subject.key # ID number or name
-        self.subject_number = subject.value
-        self.dependent_value = dependent.value
-        self.independent_value = independent.value #not coded right
+    def __init__(self, subject: dict, dependent: int, independent: int): # subject dict will be {'name': 'x', 'ID': y}
+        self.subject_name = subject.get('name') # ID number or name
+        self.subject_number = subject.get('ID')
+        self.dependent_value = dependent
+        self.independent_value = independent
         
 class Dependent_variable: # Will need variable name, type of variable (discrete, categorical, etc)
     def __init__(self, variable_name, variable_type):
@@ -73,52 +60,27 @@ class Independent_variable:
         self.variable_name = variable_name
         self.variable_type = variable_type
 
-
-class Experiment: # Will use this to create an instance of each experiment
+class Experimental_data: # Will use this to create an instance of each experiment
     def __init__(self, dataset, dependent_variable, independent_variable):
         self.subject_IDs = []
         self.dependent_values = []
         self.independent_values = []
         self.dependent_name = ''
-        self.dependent_type = ''
+        self.dependent_type = '' # let's start with discrete variables to make it easier to calculate
         self.independent_name = ''
         self.independent_type = ''
     
 class Distribution: # We're using a class for this in case we want to reuse just the distribution data later
     # Distribution will calculate mean, sum of squares, variance, and standard deviation
-    def __init__(self): # Work in progress
-        pass
-
-    def mean(self, dataset):
-        n = len(dataset)
-        sum = 0
-        for num in dataset:
-            sum += num
-        self.mean = sum / n
-        return self.mean
-
-    def sum_of_squares(self):
-
-        pass
-
-    def variance(self):
-
-        pass
-
-    def standard_deviation(self, mean: float, dataset): # Sample standard deviation, not population
-        # I'm calculating it all here, but I'll go back and calculate them independently in the other functions
-        n = len(dataset)
-        s = 0 # standard deviation
-        sum = 0 
-        for num in dataset:
-            sum += ((num - mean) ** 2)
-        self.s = (sum / (n-1)) ** (1/2)
-        return self.s
+    def __init__(self, dataset: list): # Work in progress
+        self.mean = mean(dataset)
+        self.sum_of_squares = sum_of_squares(self.mean, dataset)
+        self.variance = variance(self.variance, dataset)
+        self.standard_deviation = standard_deviation(self.variance)
 
 class Paired_sample_t_test:
-    def __init__(self, independent: str, standard_deviation1: float, standard_deviation2: float):
 
-        pass
+    pass
 
 class Independent_t_test:
 
