32643b4c3ec75b9920f529175e46fdaa66ba499d
ezekiel wells <ezekield.wells@gmail.com>
2022-06-16 20:20:34 -0700

    lab changes

--- code/zeke_hedgehog/python/lecture/functions.py
+++ code/zeke_hedgehog/python/lecture/functions.py
@@ -43,33 +43,43 @@
 # Multiple parameter functions/ positional arguments
 
 
-def addition(num1, num2):
-    result = num1 + num2
-    return result   
+# def addition(num1, num2):
+#     result = num1 + num2
+#     return result   
 
-addition(3,4)
+# addition(3,4)
 
-total = addition(1,2)
-print(total)
+# total = addition(1,2)
+# print(total)
 
-def subtraction(num1, num2):
-    result = num1 - num2
-    return result   
+# def subtraction(num1, num2):
+#     result = num1 - num2
+#     return result   
 
-print(subtraction(3,4))
+# print(subtraction(3,4))
 
-def return_two():
-    return True, False
+# def return_two():
+#     return True, False
+
+# #----------------------------------
+# ## recursion function
+
+# def get_user_input():
+#     choices = ["rock", "paper", "scissors"]
+#     user_input = ["Choose rock, paper, or scissors"]
+#     if user_input in choices:
+#         return user_input
+#     else:
+#         return get_user_input()
+
+# print(get_user_input())
+
 
 #----------------------------------
-## recursion function
 
-def get_user_input():
-    choices = ["rock", "paper", "scissors"]
-    user_input = ["Choose rock, paper, or scissors"]
-    if user_input in choices:
-        return user_input
-    else:
-        return get_user_input()
+def addition(x,y):
+    result = x + y
 
-print(get_user_input())
+    return addition
+
+print(4,5)
