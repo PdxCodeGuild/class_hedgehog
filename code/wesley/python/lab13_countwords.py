cecf7649c57001bc411ccb5690ef30c95b303830
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-06-16 19:51:28 -0700

    lab13

--- code/wesley/python/lab13_countwords.py
+++ code/wesley/python/lab13_countwords.py
@@ -15,8 +15,7 @@ for word in new_list:
     else:
         word_dict[word] = 1
 
-# word_dict is a dictionary where the key is the word and the value is the count
-words = list(word_dict.items()) # .items() returns a list of tuples
-words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+words = list(word_dict.items()) 
+words.sort(key=lambda tup: tup[1], reverse=True)  
+for i in range(min(10, len(words))):  
     print(words[i])
