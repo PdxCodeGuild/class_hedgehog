3fe6ab644f23747c9bfba12a1044129d20389f91
Stacy Green <stacyleegreen@gmail.com>
2022-06-18 10:57:41 +0700

    Cleaned up code

--- code/stacy/python/minicap_statistical_analysis.py
+++ code/stacy/python/minicap_statistical_analysis.py
@@ -5,8 +5,8 @@ Goals:
     Display that data
 
     T-tests
-        -Paired t-test
         -Independent t-test
+        -Paired t-test
         -One sample t-test
 
 Stretch Goals:
@@ -81,6 +81,135 @@ for index, data in enumerate(subject_data):
 
 #######################################################################################################
 
+""" Classes """
+
+class Dataset:
+    def __init__(self, dataset: list):
+        self.dependent_variable_name = 'weight'
+        self.dependent_variable_type = 'ratio'
+        self.dependent_variable_data_type = 'continuous'
+        self.independent_variable_name = 'gender'
+        self.independent_variable_type = 'nominal'
+        self.independent_variable_data_type = 'discrete'
+        self.subject_IDs = []
+        for subject in dataset:
+            self.subject_IDs.append(subject.get('ID'))
+        self.dependent_values = []
+        for subject in dataset:
+            self.dependent_values.append(subject.get('dependent value'))
+        self.independent_values = []
+        for subject in dataset:
+            self.independent_values.append(subject.get('gender'))
+    
+    def print_data(self):
+        return f' Subject IDs: {self.subject_IDs} \nDependent Variable Values: {self.dependent_values} \nIndependent Variable Values: {self.independent_values}\n'
+
+    def __str__(self):
+        return f'ID: Identification number\nGender: Male = 0, Female = 1\nDependent Variable Name: {self.dependent_variable_name}\nDependent Variable Type: {self.dependent_variable_type}\nDependent Data Type: Continuous\nIndependent Variable Name: Gender\nIndependent Variable Type: Nominal\nIndependent Data Type: Discrete\n'
+
+class Distribution: 
+    def __init__(self, dependent_values: list): # Dataset will be a list of dependent variable values
+        self.mean = mean(dependent_values)
+        self.sum_of_squares = sum_of_squares(self.mean, dependent_values)
+        self.variance = variance(self.sum_of_squares, dependent_values)
+        self.standard_deviation = standard_deviation(self.variance)
+    
+    def __str__(self):
+        return f'mean: {self.mean} \nsum of squares: {self.sum_of_squares} \nvariance: {self.variance} \nSD: {self.standard_deviation}\n'
+
+    # def visualize():
+    #     pass
+        
+class Independent_t_test:
+    def __init__(self, dataset):
+        self.sample_1 = []
+        self.sample_2 = []
+        for subject in dataset:
+            if subject.get('gender') == 'male':
+                self.sample_1.append(subject.get('dependent value'))
+            elif subject.get('gender') == 'female':
+                self.sample_2.append(subject.get('dependent value'))
+        # print(self.sample_1, self.sample_2)
+
+        self.sample_1_distribution = Distribution(self.sample_1)
+        self.sample_1_mean = self.sample_1_distribution.mean
+        self.sample_1_sum_of_squares = self.sample_1_distribution.sum_of_squares
+        self.sample_1_variance = self.sample_1_distribution.variance
+        self.sample_1_standard_deviation = self.sample_1_distribution.standard_deviation
+
+        self.sample_2_distribution = Distribution(self.sample_2)
+        self.sample_2_mean = self.sample_2_distribution.mean
+        self.sample_2_sum_of_squares = self.sample_2_distribution.sum_of_squares
+        self.sample_2_variance = self.sample_2_distribution.variance
+        self.sample_2_standard_deviation = self.sample_2_distribution.standard_deviation
+
+        self.t_value = (self.sample_1_mean - self.sample_2_mean)/(((self.sample_1_variance/len(self.sample_1))+(self.sample_2_variance/len(self.sample_2)))**(1/2))
+
+        self.degrees_of_freedom = len(self.sample_1) + len(self.sample_2) - 2
+
+    def __str__(self):
+        return f'Sample 1 size: {len(self.sample_1)}\nSample 1 mean: {self.sample_1_mean}\nSample 1 SD: {self.sample_1_standard_deviation}\nSample 2 size: {len(self.sample_2)}\nSample 2 mean: {self.sample_2_mean}\nSample 2 SD: {self.sample_2_standard_deviation}\nt-value: {self.t_value}\nDegrees of Freedom: {self.degrees_of_freedom}'
+
+#########################################################################################################
+
+""" Testing """
+
+test1_data = Dataset(dataset)
+test1_independent_t_test = Independent_t_test(dataset)
+print(test1_independent_t_test)
+
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+
+""" ######################################  Works in Progress  ######################################"""
+
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+'''
+class Paired_sample_t_test:
+    def __init__(self, subject, dependent, independent, dataset):
+        self.sample_size = ''
+        self.dependent = ''
+        self.independent = ''
+        self.standard_deviation1 = ''
+        self.standard_deviation2 = ''
+        self.effect_size = ''
+        self.statistical_significance = ''
+
+    def effect_size(self):
+
+        pass
+
+    def statistical_significance(self):
+
+        pass
+
+    def visualize_distribution(self):
+
+        pass
+    
+    def __str__(self):
+
+        pass
+    
+    
+class Single_sample_t_test:
+    def __init__(self):
+    
+        pass
+    
+    def __str__(self):
+
+        pass
+
+#######################################################################################################
+
 """ Unused Classes """
 
 # class Subject:
@@ -131,123 +260,4 @@ for index, data in enumerate(subject_data):
     #     return self.standard_deviation
 
 #######################################################################################################
-
-""" Classes """
-
-class Dataset:
-    def __init__(self, dataset: list):
-        self.dependent_variable_name = 'weight'
-        self.dependent_variable_type = 'ratio'
-        self.dependent_variable_data_type = 'continuous'
-        self.independent_variable_name = 'gender'
-        self.independent_variable_type = 'nominal'
-        self.independent_variable_data_type = 'discrete'
-        self.subject_IDs = []
-        for subject in dataset:
-            self.subject_IDs.append(subject.get('ID'))
-        self.dependent_values = []
-        for subject in dataset:
-            self.dependent_values.append(subject.get('dependent value'))
-        self.independent_values = []
-        for subject in dataset:
-            self.independent_values.append(subject.get('gender'))
-    
-    def print_data(self):
-        return f' Subject IDs: {self.subject_IDs} \nDependent Variable Values: {self.dependent_values} \nIndependent Variable Values: {self.independent_values}\n'
-
-    def __str__(self):
-        return f'ID: Identification number\nGender: Male = 0, Female = 1\nDependent Variable Name: {self.dependent_variable_name}\nDependent Variable Type: {self.dependent_variable_type}\nDependent Data Type: Continuous\nIndependent Variable Name: Gender\nIndependent Variable Type: Nominal\nIndependent Data Type: Discrete\n'
-
-class Distribution: 
-    def __init__(self, dependent_values: list): # Dataset will be a list of dependent variable values
-        self.mean = mean(dependent_values)
-        self.sum_of_squares = sum_of_squares(self.mean, dependent_values)
-        self.variance = variance(self.sum_of_squares, dependent_values)
-        self.standard_deviation = standard_deviation(self.variance)
-    
-    def visualize():
-
-        pass
-    
-    def __str__(self):
-        return f'mean: {self.mean} \nsum of squares: {self.sum_of_squares} \nvariance: {self.variance} \nSD: {self.standard_deviation}\n'
-
-class Independent_t_test:
-    def __init__(self, dataset):
-        self.sample_1 = []
-        self.sample_2 = []
-        for subject in dataset:
-            if subject.get('gender') == 'male':
-                self.sample_1.append(subject.get('dependent value'))
-            elif subject.get('gender') == 'female':
-                self.sample_2.append(subject.get('dependent value'))
-        # print(self.sample_1, self.sample_2)
-
-        self.sample_1_distribution = Distribution(self.sample_1)
-        self.sample_1_mean = self.sample_1_distribution.mean
-        self.sample_1_sum_of_squares = self.sample_1_distribution.sum_of_squares
-        self.sample_1_variance = self.sample_1_distribution.variance
-        self.sample_1_standard_deviation = self.sample_1_distribution.standard_deviation
-
-        self.sample_2_distribution = Distribution(self.sample_2)
-        self.sample_2_mean = self.sample_2_distribution.mean
-        self.sample_2_sum_of_squares = self.sample_2_distribution.sum_of_squares
-        self.sample_2_variance = self.sample_2_distribution.variance
-        self.sample_2_standard_deviation = self.sample_2_distribution.standard_deviation
-
-        self.t_value = (self.sample_1_mean - self.sample_2_mean)/(((self.sample_1_variance/len(self.sample_1))+(self.sample_2_variance/len(self.sample_2)))**(1/2))
-
-        self.degrees_of_freedom = len(self.sample_1) + len(self.sample_2) - 2
-
-
-    def __str__(self):
-        return f'Sample 1 size: {len(self.sample_1)}\nSample 1 mean: {self.sample_1_mean}\nSample 1 SD: {self.sample_1_standard_deviation}\nSample 2 size: {len(self.sample_2)}\nSample 2 mean: {self.sample_2_mean}\nSample 2 SD: {self.sample_2_standard_deviation}\nt-value: {self.t_value}\nDegrees of Freedom: {self.degrees_of_freedom}'
-        
-
-#########################################################################################################
-
-""" Testing """
-test1_data = Dataset(dataset)
-test1_independent_t_test = Independent_t_test(dataset)
-print(test1_independent_t_test)
-
-#########################################################################################################
-class Paired_sample_t_test:
-    def __init__(self, subject, dependent, independent, dataset):
-        self.sample_size = ''
-        self.dependent = ''
-        self.independent = ''
-        self.standard_deviation1 = ''
-        self.standard_deviation2 = ''
-        self.effect_size = ''
-        self.statistical_significance = ''
-
-    def effect_size(self):
-
-        pass
-
-    def statistical_significance(self):
-
-        pass
-
-    def visualize_distribution(self):
-
-        pass
-    
-    def __str__(self):
-
-        pass
-    
-    
-class Single_sample_t_test:
-    def __init__(self):
-    
-        pass
-    
-    def __str__(self):
-
-        pass
-
-#######################################################################################################
-
-
+'''
