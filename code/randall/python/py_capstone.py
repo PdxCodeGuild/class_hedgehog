7a0054a27caf64954167a1085bcb5a0cfb7af70a
TheAncientBooer <randall.mayton@gmail.com>
2022-06-18 12:53:46 -0700

    Save cap draft

--- code/randall/python/py_capstone.py
+++ code/randall/python/py_capstone.py
@@ -6,17 +6,53 @@ import requests
 import creds     #import external file (creds.py added to .gitignore) which contains
                  # the API key with the variable "api_key". Keeps API key secret
 
-base_url = "http://api.openweathermap.org/data/2.5/weather"
-city = input(Fore.GREEN +"Welcome to Weather Checker. For current conditions, please enter a city name: ")
+def general_conditions():
+    """This function will return the general conditions of the selected city"""
 
-requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
-response = requests.get(requests_url)
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    city = input(Fore.GREEN +"Welcome to Weather Checker. For current conditions, please enter a city name: ")
 
-if response.status_code == 200:
-    data = response.json()
-    weather = data["weather"]
-    print(Fore.YELLOW + weather[0]["description"].title())
-    temperature = round(data["main"]["temp"] - 273.15) * 9/5 + 32 #Converts Kelvin to Fahrenheit and rounds 
-    print(f"Temperature: {temperature} °f")
-else:
-    print("Error")
+    requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
+    response = requests.get(requests_url)
+
+
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = data["weather"]
+        print(Fore.YELLOW + weather[0]["description"].title())
+        temperature = round(data["main"]["temp"] - 273.15) * 9/5 + 32 #Converts Kelvin to Fahrenheit and rounds 
+        print(f"Temperature: {temperature} °f")
+    else:
+        print("Error")
+
+
+def detailed_conditions():
+    """This function will return the detailed conditions of the weather.""" #REQUIRES A PAID API KEY
+
+    lat = input(Fore.GREEN +"Enter latitude: ")
+    lon = input(Fore.GREEN +"Enter longitude: ")
+    requests_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alerts&appid={creds.api_key}"
+    
+
+    #requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
+    response = requests.get(requests_url)
+
+    print(response)
+"""
+    if response.status_code == 200:
+        data = response.json()
+        weather = data["weather"]
+        print(Fore.YELLOW + weather[0]["description"].title())
+        temperature = round(data["main"]["temp"] - 273.15) * 9/5 + 32 #Converts Kelvin to Fahrenheit and rounds 
+        print(f"Temperature: {temperature} °f")
+    else:
+        print("Error")
+"""  
+
+user_input = input(Fore.GREEN + "Welcome to Weather Checker. Would you like to see the general or the detailed conditions? (g/d): ").lower()
+
+if user_input == "g":
+    general_conditions()
+elif user_input == "d":
+    detailed_conditions()
--- code/randall/python/test.py
+++ code/randall/python/test.py
@@ -1,58 +1,17 @@
-# Python ₘᵢₙᵢ capstone
-# Weather API
-
-from colorama import Fore  # for purdy colors
-import requests
-import creds     #import external file (creds.py added to .gitignore) which contains
-                 # the API key with the variable "api_key". Keeps API key secret
-
-def general_conditions():
-    """This function will return the general conditions of the selected city"""
-
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    city = input(Fore.GREEN +"Welcome to Weather Checker. For current conditions, please enter a city name: ")
-
-    requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
-    response = requests.get(requests_url)
+# Just test things
 
 
-
-    if response.status_code == 200:
-        data = response.json()
-        weather = data["weather"]
-        print(Fore.YELLOW + weather[0]["description"].title())
-        temperature = round(data["main"]["temp"] - 273.15) * 9/5 + 32 #Converts Kelvin to Fahrenheit and rounds 
-        print(f"Temperature: {temperature} °f")
-    else:
-        print("Error")
-
-
-def detailed_conditions():
-    """This function will return the detailed conditions of the weather.""" #REQUIRES A PAID API KEY
-
-    lat = input(Fore.GREEN +"Enter latitude: ")
-    lon = input(Fore.GREEN +"Enter longitude: ")
-    requests_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alerts&appid={creds.api_key}"
-    
-
-    #requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
-    response = requests.get(requests_url)
-
-    print(response)
 """
-    if response.status_code == 200:
-        data = response.json()
-        weather = data["weather"]
-        print(Fore.YELLOW + weather[0]["description"].title())
-        temperature = round(data["main"]["temp"] - 273.15) * 9/5 + 32 #Converts Kelvin to Fahrenheit and rounds 
-        print(f"Temperature: {temperature} °f")
-    else:
-        print("Error")
-"""  
+Black Knight: “Tis but a scratch.”
+King Arthur: “A scratch? Your arm’s off!
+King Arthur: “Look, you stupid b***ard, you’ve got no arms left!”
+Black Knight: “Yes I have.”
+King Arthur: “Look!”
+Black Knight: “It’s just a flesh wound…”
 
-user_input = input(Fore.GREEN + "Welcome to Weather Checker. Would you like to see the general or the detailed conditions? (g/d): ").lower()
+Black Knight: “Right, I’ll do you for that!”
+King Arthur: “You’ll what?”
+Black Knight: “Come here!”
+King Arthur: “What are you gonna do, bleed on me?”
 
-if user_input == "g":
-    general_conditions()
-elif user_input == "d":
-    detailed_conditions()
+"""
