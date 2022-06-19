3dc125a82a5249a3b3383654e1fad1a1d9b05501
TheAncientBooer <randall.mayton@gmail.com>
2022-06-19 10:49:29 -0700

    Save edit uv index add

--- code/randall/python/py_capstone.py
+++ code/randall/python/py_capstone.py
@@ -1,7 +1,7 @@
 # Python ₘᵢₙᵢ capstone
 # UV index and Weather API
 # Weather  ---  https://openweathermap.org/api
-# UV Index --- https://www.openuv.io/
+# UV Index ---  https://data.epa.gov/efservice/getEnvirofactsUVDAILY/ZIP
 
 from colorama import Fore  # for purdy colors
 import requests
@@ -13,11 +13,8 @@ def weather():
 
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     city = input(Fore.GREEN +"Welcome to Weather Checker. For current conditions, please enter a city name: ")
-
     requests_url = f"{base_url}?appid={creds.api_key}&q={city}" 
     response = requests.get(requests_url)
-
-
 
     if response.status_code == 200:
         data = response.json()
@@ -29,14 +26,25 @@ def weather():
         print("Error")
 
 def uv_index():
-    """This function will return the UV index of the entered coordinates"""
+    """This function will return the UV index of the entered zip code"""
 
-    base_url = "https://api.openuv.io/api/v1/uv"
-    lat = input(Fore.GREEN +"Please enter the latitude: ")
+    zip_code = input(Fore.GREEN +"Please enter the Zip Code: ")
+    requests_url = f"https://data.epa.gov/efservice/getEnvirofactsUVDAILY/ZIP/{zip_code}/JSON"
+    response = requests.get(requests_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        zip = data[0]["ZIP_CODE"]              
+        UV = data[0]["UV_INDEX"]
+        UV_ALERT = data[0]["UV_ALERT"]
+        print(zip)
+        
+    else:
+        print("Error")
 
 user_input = input(Fore.GREEN + "Welcome to Weather & UV index Checker. Would you like to see the UV index (u) or weather (w)? (u/w): ").lower()
 
-if user_input == "g":
+if user_input == "w":
     weather()
-elif user_input == "d":
+elif user_input == "u":
     uv_index()
