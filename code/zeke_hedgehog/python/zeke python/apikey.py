498aefee9585df1421db8871a78cd29674c5dd2e
ezekiel wells <ezekield.wells@gmail.com>
2022-06-23 18:03:44 -0700

    minicapestone

---
+++ code/zeke_hedgehog/python/zeke python/apikey.py
@@ -0,0 +1,12 @@
+
+#No API key is required but requests will be limited to 5 per 30 second period by IP, if
+#  you have an active ZenQuotes subscription, your key will work across all of our services.
+
+#Allows users to get a motivational encouragement when needed.
+
+
+
+apikey = "810b7e7001c52f9e5613d09f59f4215f8101677d"
+
+https://today.zenquotes.io/[month]/[day]/[your_key]
+
