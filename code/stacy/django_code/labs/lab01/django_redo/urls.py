96655b52acf882a2d766a61619edb7ace5b7bc7e
Stacy Green <stacyleegreen@gmail.com>
2022-07-14 11:30:42 +0700

    Added css to Django-Redo: Unit Converter

--- code/stacy/django_code/labs/lab01/django_redo/urls.py
+++ code/stacy/django_code/labs/lab01/django_redo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import unit_converter
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('convert/', include('unit_converter.urls'))
+    path('convert/', include('unit_converter.urls')),
 ]
