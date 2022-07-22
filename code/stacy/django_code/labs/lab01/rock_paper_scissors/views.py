0d888f8e90a2abd96c05d40732a6df24c43e8b9f
Stacy Green <stacyleegreen@gmail.com>
2022-07-22 11:03:43 +0700

    Added outline for other apps in proj

---
+++ code/stacy/django_code/labs/lab01/rock_paper_scissors/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+"""
+Let's play rock-paper-scissors with the computer. You may want to try using these emojis 🗿📃✂️✊✋✌
+
+The computer will ask the user for their choice (rock, paper, scissors)
+The computer will randomly choose rock, paper or scissors
+Determine who won and tell the user
+Let's list all the cases:
+
+rock vs rock (tie)
+rock vs paper
+rock vs scissors
+paper vs rock
+paper vs paper (tie)
+paper vs scissors
+scissors vs rock
+scissors vs paper
+scissors vs scissors (tie)
+Version 2 (optional)
+After playing, ask them if they'd like to play again. If they say yes, restart the game, otherwise exit.
+"""
