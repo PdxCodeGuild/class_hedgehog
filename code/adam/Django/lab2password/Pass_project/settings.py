e8f3ec0d7ac0cfdb2620eeabebc131538bf72337
Adam McWhorter <adamm2bits@hotmail.com>
2022-07-23 00:31:58 -0700

    password app/linked nicely with grocery(not done)

--- code/adam/Django/lab2password/Pass_project/settings.py
+++ code/adam/Django/lab2password/Pass_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'password',
-    'grocery_list'
+    'grocery_list',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'Pass_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- code/adam/Django/lab2password/Pass_project/urls.py
+++ code/adam/Django/lab2password/Pass_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('password.urls'))
+    path('', include('password.urls')),
+    path('list/', include('grocery_list.urls'))
 ]
