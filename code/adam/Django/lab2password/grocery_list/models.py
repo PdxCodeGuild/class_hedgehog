91333da6707929f53de82b06041e6085fd57fb0c
Adam McWhorter <adamm2bits@hotmail.com>
2022-07-23 17:05:34 -0700

    GroceryItem added

--- code/adam/Django/lab2password/grocery_list/models.py
+++ code/adam/Django/lab2password/grocery_list/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class GroceryItem(models.Model):
+    description = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField()
