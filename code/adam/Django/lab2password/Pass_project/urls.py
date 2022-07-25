a7fd00b891a4a33650ee55344dc5d06ef668a28c
Adam McWhorter <adamm2bits@hotmail.com>
2022-07-25 14:30:35 -0700

    issues with rendering list.details. can see the emply element on site.

--- code/adam/Django/lab2password/Pass_project/urls.py
+++ code/adam/Django/lab2password/Pass_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('password.urls')),
-    path('list/', include('grocery_list.urls'))
+    path('shopping/', include('grocery_list.urls'))
 ]
--- code/adam/Django/lab2password/grocery_list/urls.py
+++ code/adam/Django/lab2password/grocery_list/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='grocery')
+    path('', views.index, name='groceries'),
+    path('create/', views.create_list, name= 'create_list'),
 ]
