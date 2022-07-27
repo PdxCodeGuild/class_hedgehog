ecd9b508148db5f727710daf67043b199e8c6ddd
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-07-26 20:25:27 -0700

    models update

--- code/wesley/django/django_models/assignments/models.py
+++ code/wesley/django/django_models/assignments/models.py
@@ -5,7 +5,7 @@ class Assignment(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     date_assigned = models.DateField(blank=True, null=True)
-    date_due = models.DateField(blank=True, null=True)
+    date_due = models.DateField()
 
     def __str__(self):
         return self.title
