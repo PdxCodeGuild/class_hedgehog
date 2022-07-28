72146975b15a6cda6741b1d29b411872e0311842
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-07-27 20:02:56 -0700

    ToDo list

--- code/wesley/django/labs/grocery_list/urls.py
+++ code/wesley/django/labs/grocery_list/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'grocerylist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('item/<int:item_id>/', views.update, name='update')
