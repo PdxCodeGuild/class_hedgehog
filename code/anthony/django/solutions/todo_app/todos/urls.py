22d841dff2bf3831c8d211d9857b549064b62bfb
anthony <anthonyt.wallace@icloud.com>
2022-08-02 21:20:14 -0700

    Todo App completed (mostly)

--- code/anthony/django/solutions/todo_app/todos/urls.py
+++ code/anthony/django/solutions/todo_app/todos/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout")
+    path('logout/', views.logout, name="logout"),
+    path('create/', views.create, name="create"),
+    path('delete/<int:todo_id>/', views.delete, name="delete"),
+    path('update/<int:todo_id>/', views.update, name="update"),
+    path('complete/<int:todo_id>/', views.complete, name="complete")
 ]
 
 
