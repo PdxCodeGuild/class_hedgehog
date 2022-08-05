63c47511135011d355cf92310b7a6bebf1e1ca94
Adam McWhorter <adamm2bits@hotmail.com>
2022-08-05 00:00:09 -0700

    forms/models in admin

--- code/adam/Django/lab4blog/blog/views.py
+++ code/adam/Django/lab4blog/blog/views.py
@@ -1,25 +1,35 @@
 from django.shortcuts import render
+from blog.models import BlogPost, User
+from .forms import BlogForm
+
 
 # Create your views here.
 
-def loginPage(requests):
+def loginPage(request):
 
     context = {}
-    return render(requests, "blog/login_register.html", context)
+    return render(request, "blog/login_register.html", context)
 
 
 
-def index(requests):
+def index(request):
     msg = "Turtle Island is great, so many turtles here."
 
     context = {
         "msg": msg
     }
-    return render(requests, 'blog/index.html', context)
+    return render(request, 'blog/index.html', context)
 
 
 
-def createPost(requests):
-    context = {}
+def createPost(request):
+    form = BlogForm()
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        print(request.POST)
+            
+    context = {
+        "form": form
+    }
 
-    return render(requests, 'blog/create_post.html', context)
+    return render(request, 'blog/create_post.html', context)
