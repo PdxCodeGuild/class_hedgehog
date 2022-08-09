b720f48605ad37b7a669c2fdf6cf6a61d7b80811
Stacy Green <stacyleegreen@gmail.com>
2022-08-09 11:02:22 +0700

    Completed lab01 Version 2

--- code/stacy/javascript/labs/lab01/lab01.js
+++ code/stacy/javascript/labs/lab01/lab01.js
@@ -1,15 +1,4 @@
 // Lab 1: JavaScript Redo
-// Version 1
-// Pick a Python lab and re-do it in JavaScript. You should first try to write them using JavaScript's prompt and alert in place of Python's input and print.
-
-// Unit Converter
-
-// Version 2 (optional)
-// Once you have that working, use input and button elements, with events. You can read the docs on DOM Manipulation and Events. You can view a demo here.
-
-
-// measurement_in_meters = measurement * unit_conversions.get(input_unit, 1)
-// converted_measurement = measurement_in_meters / unit_conversions.get(output_unit, 1)
 
 const unitConversions = {
     ft: "0.3048",
@@ -20,14 +9,33 @@ const unitConversions = {
     in: "0.0254"  
 }
 
-let inputNumber = Number(prompt("Enter meausurement \n\t> "))
-// console.log(inputNumber)
-let inputUnit = prompt("Enter input unit \n\t> ")
-// console.log(inputUnit)
-let outputUnit = prompt("Enter output unit \n\t> ")
-// console.log(outputUnit)
-let measurementInMeters = inputNumber * unitConversions[inputUnit]
-// console.log(measurementInMeters)
-let outputNumber = measurementInMeters / unitConversions[outputUnit]
-// console.log(outputNumber)
-alert(`${inputNumber} ${inputUnit} is ${outputNumber} ${outputUnit}`)
+//////////////// Version 1 ////////////////
+
+// let inputNumber = Number(prompt("Enter meausurement \n\t> "))
+// // console.log(inputNumber)
+// let inputUnit = prompt("Enter input unit \n\t> ")
+// // console.log(inputUnit)
+// let outputUnit = prompt("Enter output unit \n\t> ")
+// // console.log(outputUnit)
+// let measurementInMeters = inputNumber * unitConversions[inputUnit]
+// // console.log(measurementInMeters)
+// let outputNumber = measurementInMeters / unitConversions[outputUnit]
+// // console.log(outputNumber)
+// alert(`${inputNumber} ${inputUnit} is ${outputNumber} ${outputUnit}`)
+
+
+//////////////// Version 2 ////////////////
+
+let inputNumber = document.querySelector('#inputNumber');
+let inputUnit = document.querySelector('#inputUnit');
+let outputUnit = document.querySelector('#outputUnit');
+let run_bt = document.querySelector('#run_bt');
+let output_div = document.querySelector('#output_div');
+run_bt.onclick = function() {
+    let inputNumberValue = Number(inputNumber.value);
+    let inputUnitValue = inputUnit.value;
+    let outputUnitValue = outputUnit.value
+    let measurementInMeters = inputNumberValue * unitConversions[inputUnitValue]
+    let convertedNumber = measurementInMeters / unitConversions[outputUnitValue]
+    output_div.innerText = inputNumberValue + ' ' + inputUnitValue + ' is ' + convertedNumber + ' ' + outputUnitValue;
+}
