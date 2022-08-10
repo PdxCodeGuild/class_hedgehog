47db1320198901bc1eaf0f05010a516a623a51b2
Zane <zanecashby@gmail.com>
2022-08-10 00:07:44 -0400

    Added ROT_13

---
+++ code/ashby/JavaScript/rock_paper_scissors/rot_13.js
@@ -0,0 +1,31 @@
+let input_message = document.querySelector('#input_message');
+
+let input_rotation = document.querySelector('#input_rotation');
+
+let encrypt_button = document.querySelector('#encrypt_button'); 
+
+let encrypted_message = document.querySelector('#encrypted_message');   
+
+
+encrypt_button.onclick = function(){ alert('starting')
+let    rotatedPhrase = '';
+const alphabets = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '1234567890', '!@#$%^&*()+=+,-./'];
+let    inputMessage = input_message.value;
+let    inputRotation = Number(input_rotation.value);
+    for(let letter of inputMessage){
+        for(let alphabet of alphabets){
+            if (alphabet.includes(letter)){
+                const index = alphabet.indexOf(letter);
+                const rotatedIndex = ((index + inputRotation) % alphabet.length)
+                const rotatedLetter = alphabet[rotatedIndex];
+                console.log(rotatedLetter)
+                rotatedPhrase += rotatedLetter
+                
+            } else if(letter == ' '){ rotatedPhrase += ' '}
+        }
+        
+    }
+    console.log(rotatedPhrase)
+    encrypted_message.innerText = rotatedPhrase;
+
+}
