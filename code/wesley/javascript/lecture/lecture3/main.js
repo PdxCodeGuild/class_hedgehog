b61cb3370ab256961d5ce792261b5a6b1d8aeceb
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-08-10 20:36:58 -0700

    lecture 3

---
+++ code/wesley/javascript/lecture/lecture3/main.js
@@ -0,0 +1,8 @@
+
+const userInput = document.querySelector('#user-input')
+const button = document.querySelector('#submit')
+
+button.addEventListener('click', function(){
+    console.log(userInput.value)
+
+})
