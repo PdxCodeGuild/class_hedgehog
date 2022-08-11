13f06a11f7b1613cc1f793bcb4ddb857ddb390f9
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-08-10 18:10:14 -0700

    update lab1

--- code/wesley/javascript/first_js/labs/lab1/ROT.js
+++ code/wesley/javascript/first_js/labs/lab1/ROT.js
@@ -1,24 +1,28 @@
 
+let phrase_input = document.querySelector('#userString');
+let rot_input = document.querySelector('#rotateAmount');
+let run_bt = document.querySelector('#run');
+let output = document.querySelector('#output');
 
-const userString = prompt('Enter the string: ')
-let rotateAmount = prompt('Enter amount to rotate:')
-rotateAmount = parseInt(rotateAmount)
-const lowerUserString = userString.toLowerCase()
-const lowercase = ['abcdefghijklmnopqrstuvwxyz']
 
-function rotatePhrase(userString){
+run_bt.onclick = function(){
+    const userString = phrase_input.value
+    let rotateAmount = rot_input.value
+    rotateAmount = parseInt(rotateAmount)
+    const lowerUserString = userString.toLowerCase()
+    const lowercase = ['abcdefghijklmnopqrstuvwxyz']
+    
+    
     let rot = ''
     for (char of userString){
         for (let char2 of lowercase){
-           if (char2.includes(char)){
-               const location = char2.indexOf(char)
-               const rotatedIn = (location + rotateAmount) % char2.length
-               const rotatedLet = char2[rotatedIn]
-               rot += rotatedLet
+            if (char2.includes(char)){
+                const location = char2.indexOf(char)
+                const rotatedIn = (location + rotateAmount) % char2.length
+                const rotatedLet = char2[rotatedIn]
+                rot += rotatedLet
             }
         }
     }
-    return rot
-    
+    output.innerText = rot
 }
-console.log(rotatePhrase(userString))
