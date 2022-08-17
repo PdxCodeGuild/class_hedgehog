e2b8cf956d34d909ec9d94b0fcd299c20dffbf0e
Keenen_Pendergraft <keenen.l.pendergraft@gmail.com>
2022-08-16 23:55:17 -0400

    Finished lab4 dad jokes part2

---
+++ code/keenen/javascript/labs/lab04_dadjoke/main.js
@@ -0,0 +1,37 @@
+
+const search = document.querySelector('#search')
+const btnJoke = document.querySelector('#btn-joke')
+const displayJokeDiv = document.querySelector('#display-joke')
+
+
+
+
+btnJoke.addEventListener("click", function(){
+    fetch(`https://icanhazdadjoke.com/search?term=${search.value}`, {
+  headers: {
+    accept: "application/json"
+  }
+}).then(response => response.json())
+.then(data => generateJoke(data))
+}
+)
+
+
+
+function generateJoke(data) {
+   console.log(data)
+    if(data.results){
+        displayJokeDiv.textContent = `${data.results[0].joke}`
+    }
+    else {
+        displayJokeDiv.textContent = 'Joke 404'
+    }
+
+    // for(let jokes of data.joke){
+    // const joke = document.createElement('joke')
+    // displayJokeDiv.appendChild(jokes)    
+
+    // console.log(joke)
+
+    }
+
