1fbb3d710b005f8d1e7c65c3546ed6661144a466
ZAshby <zanecashby@gmail.com>
2022-08-16 22:47:23 -0400

    updated lab_04 version 2

--- code/ashby/JavaScript/Lab_04 Dad_Joke/main.js
+++ code/ashby/JavaScript/Lab_04 Dad_Joke/main.js
@@ -1,5 +1,7 @@
 const dadContainer = document.querySelector('#dad-container');
-const moreDad = document.querySelector('#more-dad');
+const randomDad = document.querySelector('#random-dad');
+const searchDad = document.querySelector('#search-dad');
+const dadInput = document.querySelector('#dad-input');
 
 
 function createDad(joke){
@@ -9,6 +11,16 @@ function createDad(joke){
     dadContainer.appendChild(h);
 }
 
+function multiDad(jokes){
+    dadContainer.innerHTML = ''
+    for(let joke of jokes){
+        let h = document.createElement('h4');
+        h.innerText = joke.joke;
+        dadContainer.appendChild(h);
+        let br = document.createElement('br');
+        dadContainer.appendChild(br);
+    }
+}
 
 async function findDad(){
     const response = await fetch('https://icanhazdadjoke.com/', {
@@ -23,4 +35,22 @@ async function findDad(){
 findDad()
 
 
-moreDad.onclick = () => findDad()
+async function searchFunction(){
+    let input = dadInput.value
+    if(!input){
+        alert('Daddy requires a valid input!')
+    } else{
+    const response = await fetch(`https://icanhazdadjoke.com/search?term=${input}&limit=5`, {
+        headers: {
+            accept: "application/json"
+        }})
+    const data = await response.json()
+    console.log(data);
+    multiDad(data.results)
+}}
+
+
+
+randomDad.onclick = () => findDad()
+
+searchDad.onclick = () => searchFunction()
