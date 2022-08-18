80ef866aad39fdff29a67db6b57d21eb8a19d3bb
Stacy Green <stacyleegreen@gmail.com>
2022-08-18 11:38:00 +0700

    Added lab04: Weather API

---
+++ code/stacy/javascript/labs/lab05_Weather/main.js
@@ -0,0 +1,41 @@
+APIKEY = ''
+
+const currentWeather = document.querySelector("#weather");
+
+// navigator.geolocation.getCurrentPosition(position => {
+//     console.log("Latitude: " + position.coords.latitude)
+//     console.log("Longitude: " + position.coords.longitude)
+// });
+
+async function getWeather() {
+    try {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=13.7363456&lon=100.663296&exclude=hourly,daily&appid=${APIKEY}`);
+    // console.log(response);
+    let data = await response.json();
+    // console.log(data);
+    let unixTimeStamp = data.current.dt;
+    // console.log(unixTimeStamp);
+    let datetime = new Date(unixTimeStamp*1000);
+    // console.log(datetime);
+    let icon = data.current.weather[0].icon;
+    // console.log(weatherIcon);
+    let weatherDescription = data.current.weather[0].description;
+    // console.log(description);
+    let img = document.createElement('img');
+    img.src = `http://openweathermap.org/img/wn/${icon}.png`;
+    let time = document.createElement("div");
+    time.innerText = datetime;
+    let description = document.createElement("div");
+    description.innerText = weatherDescription;
+    currentWeather.append(time, img, description);
+    return data;
+    } catch (err) {
+        console.log(err)
+    };
+    console.log(data);
+};
+
+let data = getWeather().then((data) => console.log(data));
+
+
+
