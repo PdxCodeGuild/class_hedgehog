cb3f8af6c4a92bf91490eae6d2445751fefe131f
Stacy Green <stacyleegreen@gmail.com>
2022-08-19 08:27:56 +0700

    Updated lab04 with API key link

--- code/stacy/javascript/labs/lab05_Weather/main.js
+++ code/stacy/javascript/labs/lab05_Weather/main.js
@@ -1,26 +1,19 @@
-APIKEY = ''
-
 const currentWeather = document.querySelector("#weather");
 
-// navigator.geolocation.getCurrentPosition(position => {
-//     console.log("Latitude: " + position.coords.latitude)
-//     console.log("Longitude: " + position.coords.longitude)
+// let position = navigator.geolocation.getCurrentPosition(position => {
+//         console.log("Latitude: " + position.coords.latitude);
+//         console.log("Longitude: " + position.coords.longitude);
+//         return position
 // });
 
 async function getWeather() {
     try {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=13.7363456&lon=100.663296&exclude=hourly,daily&appid=${APIKEY}`);
-    // console.log(response);
-    let data = await response.json();
-    // console.log(data);
-    let unixTimeStamp = data.current.dt;
-    // console.log(unixTimeStamp);
-    let datetime = new Date(unixTimeStamp*1000);
-    // console.log(datetime);
-    let icon = data.current.weather[0].icon;
-    // console.log(weatherIcon);
-    let weatherDescription = data.current.weather[0].description;
-    // console.log(description);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=37&lon=100&exclude=hourly,daily&appid=${AnthonyKey}`); // console.log(response);
+    let data = await response.json(); // console.log(data);
+    let unixTimeStamp = data.current.dt; // console.log(unixTimeStamp);
+    let datetime = new Date(unixTimeStamp*1000); // console.log(datetime);
+    let icon = data.current.weather[0].icon; // console.log(weatherIcon);
+    let weatherDescription = data.current.weather[0].description; // console.log(description);
     let img = document.createElement('img');
     img.src = `http://openweathermap.org/img/wn/${icon}.png`;
     let time = document.createElement("div");
