d7beec888fc24326a2a55cee6b85355b50f4dc3b
Stacy Green <stacyleegreen@gmail.com>
2022-08-31 06:37:25 +0700

    Added lab06: Pokedex Part3

--- code/stacy/javascript/labs/lab06_Pokedex/pokeapp/views.py
+++ code/stacy/javascript/labs/lab06_Pokedex/pokeapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Pokemon
+import json
 
 # Create your views here.
+def index(request):
+
+    return 
+
+def pokemon(request):
+    # Create a view pokemon that gets a list of Pokemon out of the database and turns them into a dictionary to be passed to JsonResponse. Verify this works by going to localhost:8000/pokemon/ in your browser and seeing a list of pokemon in JSON
+    pokemon = Pokemon.objects.all()
+    data = list(pokemon.values("number", "name"))
+    return JsonResponse({"data": data}, safe=False)
