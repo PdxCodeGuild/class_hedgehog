4a47c174a43cfd120415681653cf3b8ec5eda057
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2022-09-01 15:15:44 -0700

    added types

--- code/wesley/javascript/pokedex/pokeapp/views.py
+++ code/wesley/javascript/pokedex/pokeapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Pokemon
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -8,8 +9,11 @@ def index(request):
 
 def pokemon(request):
     pokemon = Pokemon.objects.all()
+    # paginator = Paginator(pokemon, 20)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
     poke = list(pokemon.values('number', 'name', 'image_front'))
-    return JsonResponse({'poke': poke}, safe=False)
+    return JsonResponse({'poke': poke},  safe=False)
 
 def fetchPokemon (request, search):
     if request.method == "GET":
