e44615de59c72c9a82cf4569f8b33c4c964ed4c3
Adam McWhorter <adamm2bits@hotmail.com>
2022-09-02 16:50:39 -0700

    omg i can see my pokemon!

--- code/adam/javascript/lab6dex/pokeapp/models.py
+++ code/adam/javascript/lab6dex/pokeapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
 class Pokemon(models.Model):
     number = models.IntegerField()
     name = models.CharField(max_length=50)
     height = models.FloatField()
-    wight = models.FloatField()
+    weight = models.FloatField()
     image_front = models.CharField(max_length=300)
     image_back = models.CharField(max_length=300)
     # types = models.CharField()
