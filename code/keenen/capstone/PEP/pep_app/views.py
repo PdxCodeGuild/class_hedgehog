ef2b11bec07b4b810be77eb09fd71c3da69146aa
Keenen_Pendergraft <keenen.l.pendergraft@gmail.com>
2022-09-29 16:59:49 -0400

    clean up css, signup, added "about"

--- code/keenen/capstone/PEP/pep_app/views.py
+++ code/keenen/capstone/PEP/pep_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 def index(request):
     return render (request, 'pep_app/index.html')
@@ -10,6 +13,14 @@ def about(request):
 
 def login(request):
     return render(request, 'pep_app/login.html')
+
+def signup(request):
+    return render(request, 'pep_app/signup.html')
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration.signup.html"
 
 def examiner(request):
         state_id = request.POST.get('states', None)
